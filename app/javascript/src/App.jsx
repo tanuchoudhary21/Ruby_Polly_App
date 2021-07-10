0f48de9aba6413fdfba9f8053f14a9615781acdf
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-11 00:13:25 +0530

    Added Reuseable components

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { initializeLogger } from "common/logger";
 import { setAuthHeaders } from "apis/axios";
+import Container from "./components/Container";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -14,8 +16,8 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/" component={Container} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
