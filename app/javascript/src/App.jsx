eeb8863b47227e5ae851a9a07cf4d7d47459b0e1
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-10 22:09:09 +0530

    Set up react environment

---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { initializeLogger } from "common/logger";
+import { setAuthHeaders } from "apis/axios";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    initializeLogger();
+    setAuthHeaders(setLoading);
+  }, []);
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/about" render={() => <div>About</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
