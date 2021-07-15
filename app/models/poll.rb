a7ae6434980d7e106993281054a84390b9188153
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-15 12:19:01 +0530

    Updated files

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,7 +1,7 @@
 class Poll < ApplicationRecord
     belongs_to :user
     has_many :options, dependent: :destroy
-    # has_many :votes, dependent: :destroy
+    has_many :votes, dependent: :destroy
     accepts_nested_attributes_for :options
 
     validates :title, presence: true, length: { maximum: 150 }
